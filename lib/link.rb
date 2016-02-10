1b911bce724fd178870eb6f1b40a545ba61347ac
Natacha Beck <natacha.beck@mcgill.ca>
2016-02-10 01:25:16 -0500

    Improve code readability and complete UdaciListErrors.

--- lib/link.rb
+++ lib/link.rb
@@ -4,14 +4,17 @@ class LinkItem
 
   def initialize(url, options={})
     @description = url
-    @site_name = options[:site_name]
+    @site_name   = options[:site_name]
   end
+
   def format_description
     "#{@description}".ljust(25)
   end
+  
   def format_name
     @site_name ? @site_name : ""
   end
+  
   def details
     format_description + "site name: " + format_name
   end
--- lib/todo.rb
+++ lib/todo.rb
@@ -1,25 +1,31 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class TodoItem
   # include Listable
   attr_reader :description, :due, :priority
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
-    @priority = options[:priority]
+    @due         = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @priority    = options[:priority]
   end
+
   def format_description
     "#{@description}".ljust(25)
   end
+
   def format_date
     @due ? @due.strftime("%D") : "No due date"
   end
+
   def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
+    return " ⇧" if  @priority == "high"
+    return " ⇨" if  @priority == "medium"
+    return " ⇩" if  @priority == "low"
+    return ""   if !@priority
   end
+
   def details
     format_description + "due: " +
     format_date +
